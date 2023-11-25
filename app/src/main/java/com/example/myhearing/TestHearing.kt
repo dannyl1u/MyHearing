8c7ec29c1a9448a16b3789a858d5a497eb12e038
Leon Lee <leonlee@LeonMBP.lan>
2023-11-24 20:13:43 -0800

    creating hearing test image grid

---
+++ app/src/main/java/com/example/myhearing/TestHearing.kt
@@ -0,0 +1,14 @@
+package com.example.myhearing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class TestHearing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_hearing)
+    }
+}
