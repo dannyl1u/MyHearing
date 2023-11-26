cd67ac95fa95d3e05b4d53fc2dedc0d0e3be4f3b
Leon Lee <leonlee@LeonMBP.lan>
2023-11-25 23:41:15 -0800

    improved audio, and developer's notes for next step

--- app/src/main/java/com/example/myhearing/TestHearing.kt
+++ app/src/main/java/com/example/myhearing/TestHearing.kt
@@ -2,14 +2,18 @@ package com.example.myhearing
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class TestHearing : AppCompatActivity() {
     private lateinit var StartButton : Button
+    private lateinit var DevNoteButton : Button
+
     private lateinit var mediaPlayer: MediaPlayer
     private val clickedImageIds = mutableListOf<String>()
 
@@ -17,6 +21,7 @@ class TestHearing : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.test_hearing)
         StartButton = findViewById(R.id.startButton)
+        DevNoteButton = findViewById(R.id.devNote)
 
         // Initialize MediaPlayer
         mediaPlayer = MediaPlayer()
@@ -25,6 +30,17 @@ class TestHearing : AppCompatActivity() {
             // Play the audio files in sequence
             playAudio(R.raw.queen)
         }
+        DevNoteButton.setOnClickListener {
+            val devMsg = listOf(
+                "Next step for this page:",
+                "1) Test left/right ear separately",
+                "2) Randomized audio and noise level",
+                "3) Clearer audio"
+            )
+            showSequentialSnackbar(devMsg)
+        }
+
+
     }
     private fun playAudio(audioResource: Int) {
         if (mediaPlayer.isPlaying) {
@@ -38,12 +54,12 @@ class TestHearing : AppCompatActivity() {
             // This listener is called when the audio playback is complete
             // You can add logic here to play the next audio file in sequence
             when (audioResource) {
-                R.raw.queen -> playAudio(R.raw.cat)
-                R.raw.cat -> playAudio(R.raw.king)
-                R.raw.king -> {
+                R.raw.queen -> playAudio(R.raw.rat)
+                R.raw.rat -> playAudio(R.raw.jar)
+                R.raw.jar -> {
                     // This is the last audio file in the sequence
                     // Add any additional logic or actions here
-                    showToast("All audio files played")
+//                    showToast("")
                 }
             }
         }
@@ -75,15 +91,32 @@ class TestHearing : AppCompatActivity() {
 
         if (clickedImageIds.size == 3) {
             // check answer
-            if (clickedImageIds[0] == "queen" && clickedImageIds[1]=="cat" && clickedImageIds[2]=="king") {
+            if (clickedImageIds[0] == "queen" && clickedImageIds[1]=="rat" && clickedImageIds[2]=="jar") {
                 showToast("3/3 correct!")
             } else {
                 showToast("incorrect answer.")
             }
+            clickedImageIds.clear()
         }
+
     }
 // queen > cat > king > rat > frog > door > jar > dog > car
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+
+    private fun showSequentialSnackbar(messages: List<String>) {
+        messages.forEachIndexed { index, message ->
+            Handler().postDelayed({
+                val snackbar = Snackbar.make(
+                    findViewById(android.R.id.content),
+                    message,
+                    if (index == messages.size - 1) Snackbar.LENGTH_LONG else Snackbar.LENGTH_SHORT
+                )
+                snackbar.show()
+            }, index * 3000L) // Delay between messages (adjust as needed)
+        }
     }
 }
