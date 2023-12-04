8c577791811dbb1617e7637a60d00f427758d0ae
cjtorunski <cjtorunski@gmail.com>
2023-12-03 21:14:13 -0800

    Finished calibration UI and added CountDownTimer

--- app/src/main/java/com/example/myhearing/CalibrationActivity.kt
+++ app/src/main/java/com/example/myhearing/CalibrationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myhearing
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
@@ -11,30 +12,38 @@ import android.media.AudioRecord
 import android.media.MediaRecorder.AudioSource
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.os.CountDownTimer
 import android.provider.Settings
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.LinearInterpolator
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.myhearing.databinding.ActivityCalibrationBinding
+import kotlin.math.ceil
 import kotlin.math.pow
 
 // TODO: Tune RECORD_DURATION_MS, RECORD_UPDATE_INTERVAL_MS, and DEFAULT_DECIBELS
-// TODO: Add button and timer visualization
-// TODO: Add use info for user
 
 class CalibrationActivity : ComponentActivity() {
     companion object {
         const val RECORD_DURATION_MS = 10000L
-        const val RECORD_UPDATE_INTERVAL_MS = 1000L
-        const val DEFAULT_DECIBELS = 20.0
+        const val RECORD_UPDATE_INTERVAL_MS = 500L
+        const val DEFAULT_DECIBELS = 10.0
     }
 
     private lateinit var binding: ActivityCalibrationBinding
 
-    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var cdt: CountDownTimer
     private var audioRecord: AudioRecord? = null
+    private var isCalibrating = false
+
+    private var repeatsLeft = 8
+    private var animationShouldStop = false
 
     private val maxAmplitudeData = ArrayList<Short>()
 
@@ -49,8 +58,6 @@ class CalibrationActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-
-        handler.removeCallbacksAndMessages(null)
 
         audioRecord?.stop()
         audioRecord?.release()
@@ -86,10 +93,6 @@ class CalibrationActivity : ComponentActivity() {
             audioFormat,
             bufferSize
         )
-
-        audioRecord?.startRecording()
-
-        startCalibrationRunnable()
     }
 
     private fun launchRecordAudioDialog() {
@@ -119,26 +122,70 @@ class CalibrationActivity : ComponentActivity() {
         startActivity(intent)
     }
 
-    private fun startCalibrationRunnable() {
-        val startTime = System.currentTimeMillis()
+    @SuppressLint("SetTextI18n")
+    private fun startCalibrationTimer() {
+        audioRecord?.startRecording()
 
-        val calibrationRunnable = object : Runnable {
-            override fun run() {
+        binding.calibrationBtnTimerControl.text = "CANCEL"
+
+        cdt = object : CountDownTimer(RECORD_DURATION_MS, RECORD_UPDATE_INTERVAL_MS) {
+            override fun onTick(millisUntilFinished: Long) {
                 updateCalibrationData()
 
-                val timeElapsed = System.currentTimeMillis() - startTime
-                binding.calibrationTvTimer.text = (timeElapsed / 1000).toString()
+                val secondsRemaining = ceil(millisUntilFinished / 1000.0).toInt()
+                binding.calibrationTvTimer.text = "00:${String.format("%02d", secondsRemaining)}"
+                binding.calibrationPbTimer.progress =
+                    (100 * secondsRemaining / (RECORD_DURATION_MS / 1000)).toInt()
+            }
 
-                if (timeElapsed > RECORD_DURATION_MS) {
-                    saveCalibrationFactor()
-                    handler.removeCallbacksAndMessages(null)
-                } else {
-                    handler.postDelayed(this, RECORD_UPDATE_INTERVAL_MS)
-                }
+            override fun onFinish() {
+                saveCalibrationFactor()
+
+                binding.calibrationTvTimer.text = "00:00"
+                binding.calibrationPbTimer.progress = 0
+
+                val fadeIn = AlphaAnimation(0f, 1f)
+                fadeIn.duration = 250
+                fadeIn.interpolator = LinearInterpolator()
+
+                val fadeOut = AlphaAnimation(1f, 0f)
+                fadeOut.startOffset = fadeIn.duration
+                fadeOut.duration = 250
+                fadeOut.interpolator = LinearInterpolator()
+
+                val tvAnimation = AnimationSet(false)
+                tvAnimation.addAnimation(fadeIn)
+                tvAnimation.addAnimation(fadeOut)
+
+                repeatsLeft = 8
+                animationShouldStop = false
+
+                tvAnimation.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {
+                        repeatsLeft -= 1
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        if (!animationShouldStop) {
+                            if (repeatsLeft > 1) {
+                                binding.calibrationTvTimer.startAnimation(tvAnimation)
+                            } else {
+                                resetTimer()
+                            }
+                        }
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+                        // pass
+                    }
+
+                })
+
+                binding.calibrationTvTimer.startAnimation(tvAnimation)
             }
         }
 
-        handler.post(calibrationRunnable)
+        cdt.start()
     }
 
     private fun updateCalibrationData() {
@@ -158,5 +205,31 @@ class CalibrationActivity : ComponentActivity() {
         val editor = getSharedPreferences("com.example.myhearing", Context.MODE_PRIVATE).edit()
         editor.putFloat("calibration_factor", calibrationFactor.toFloat())
         editor.apply()
+
+        Toast.makeText(this, "Device Calibrated", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onTimerControlClick(view: View) {
+        if (isCalibrating) {
+            cdt.cancel()
+            resetTimer()
+        } else {
+            isCalibrating = true
+            startCalibrationTimer()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun resetTimer() {
+        audioRecord?.stop()
+
+        binding.calibrationBtnTimerControl.text = "START"
+        binding.calibrationTvTimer.text = "00:${String.format("%02d", RECORD_DURATION_MS / 1000)}"
+        binding.calibrationPbTimer.progress = 100
+        binding.calibrationTvTimer.clearAnimation()
+
+        isCalibrating = false
+        animationShouldStop = true
+
     }
 }
